4eee5c711635d686be9774252c74afd39b93d7f0
João Bispo <joaobispo@gmail.com>
2021-03-23 10:20:24 +0000

    Semantic analysis src-lib update

--- src-lib/pt/up/fe/comp/TestUtils.java
+++ src-lib/pt/up/fe/comp/TestUtils.java
@@ -1,10 +1,14 @@
 package pt.up.fe.comp;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.StringReader;
 import java.lang.reflect.Method;
 import java.util.Properties;
 import java.util.List;
-import java.util.Scanner;
 
 
 import pt.up.fe.comp.jmm.JmmParserResult;
@@ -41,17 +45,8 @@ public class TestUtils {
 
             // It is expected that the Parser class can be instantiated without arguments
 			JmmParser parser = (JmmParser) parserClass.getConstructor().newInstance();
-
-			File file = new File(code);
-			Scanner scanner = new Scanner(file);
-
-			StringBuilder contents = new StringBuilder();
-			while (scanner.hasNextLine()) {
-				contents.append(scanner.nextLine()).append("\n");
-
-			}
-
-			return parser.parse(contents.toString());
+			
+			return parser.parse(code);
 
         } catch (Exception e) {
 			throw new RuntimeException("Could not parse code", e);
